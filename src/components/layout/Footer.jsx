c870f6972c44b02825cf0f07907e3dfecb3d9882
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-02-07 16:20:33 -0500

    set up tailwind & navbar-footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,27 @@
+import { FiTwitter, FiGithub, FiLinkedin, FiInstagram } from 'react-icons/fi'
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center gap-4 p-4 text-neutral-content lg:flex-row justify-between px-20">
+      <div className="items-center justify-center grid-flow-col">
+        <p className='text-xs'>Design & Build by Sebastian "Sanki0" Ferreyra</p>
+      </div>
+      <div className="flex justify-center gap-8 md:place-self-center md:justify-self-end">
+        <a href='https://twitter.com/SebasFC14' rel="noreferrer" target="_blank">
+          <FiTwitter className='h-6 w-6' />
+        </a>
+        <a href='https://github.com/Sanki0' rel="noreferrer" target="_blank">
+          <FiGithub className='h-6 w-6' />
+        </a>
+        <a href='https://www.linkedin.com/in/sebastian-ferreyra-4a0a47201/' rel="noreferrer" target="_blank">
+          <FiLinkedin className='h-6 w-6' />
+        </a>
+        <a href='https://www.instagram.com/iamsanki0/' rel="noreferrer" target="_blank">
+          <FiInstagram className='h-6 w-6' />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
